bd9029d48134e2f3d2d3312c18396fa1eebe1a52
TIAGO SILVA <frost.tiago@gmail.com>
2021-03-04 22:12:53 -0300

    Adicionar arquivos de projeto.

---
+++ TesteNumeroNegativo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TesteNumeroNegativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero_qualquer;
+
+            Console.WriteLine("Digite um número qualquer: ");
+            numero_qualquer = Convert.ToDouble(Console.ReadLine());
+            // 0
+
+            if(numero_qualquer > 0)
+            {
+                Console.WriteLine("Você digitou um número positivo.");
+
+            } else if(numero_qualquer < 0)
+            {
+                Console.WriteLine("Você digitou um número negativo.");
+
+            } else
+            {
+                Console.WriteLine("Você digitou zero.");
+            } 
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
